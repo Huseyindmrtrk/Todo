17bb304eec29a80721a4b3d6db4a3b2f4196c1a3
huseyindemirturk <hdemirturk@cyh.com.tr>
2024-09-17 17:20:02 +0300

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Hüseyin Demirtürk on 17.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
