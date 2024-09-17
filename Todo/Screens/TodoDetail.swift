c3d53b21b86c76e093cab6cab86e647f2a6c1209
huseyindemirturk <hdemirturk@cyh.com.tr>
2024-09-17 17:50:26 +0300

    Anasayfa ve temel ekleme yapısı yapıldı

---
+++ Todo/Screens/TodoDetail.swift
@@ -0,0 +1,18 @@
+//
+//  TodoDetail.swift
+//  Todo
+//
+//  Created by Hüseyin Demirtürk on 17.09.2024.
+//
+
+import SwiftUI
+
+struct TodoDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoDetail()
+}
